9e23483c0e79abf70f1e1f16f6a3ea6c68354dbd
Ayoub Mohamed <74332267+powplowdevs@users.noreply.github.com>
2025-04-29 10:54:13 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -161,11 +161,11 @@ const CONFIG = {
     },
   ],
   // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  },
+  //blog: {
+  //  source: 'dev', // medium | dev
+  //  username: 'arifszn', // to hide blog section, keep it empty
+  //  limit: 2, // How many articles to display. Max is 10.
+  //},
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
