1aec52435e476f41e4aeb5cfbd45c77ea374f209
Ayoub Mohamed <74332267+powplowdevs@users.noreply.github.com>
2025-04-29 19:14:59 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -175,7 +175,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'luxury',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
